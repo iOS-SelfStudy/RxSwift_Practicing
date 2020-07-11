381d066adc57e9daf26b94b5a547aadb1e0b89a2
Mohamed <mohammed.korany.ali@gmail.com>
2020-07-12 01:40:00 +0200

    versi app first screen done

---
+++ RxPracticing/Versi App/RoundBoarderTextField.swift
@@ -0,0 +1,21 @@
+//
+//  RoundBoarderTextField.swift
+//  RxPracticing
+//
+//  Created by Mohamed Korany Ali on 7/11/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import UIKit
+
+class RoundBoarderTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
